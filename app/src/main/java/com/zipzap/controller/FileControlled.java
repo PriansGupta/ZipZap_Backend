fb926299f9b08edd9cb7a5304434583cc23fef02
Priyansh Gupta <87969057+PriansGupta@users.noreply.github.com>
2026-04-12 13:03:49 +0530

    testing

--- app/src/main/java/com/zipzap/controller/FileControlled.java
+++ app/src/main/java/com/zipzap/controller/FileControlled.java
@@ -56,7 +56,7 @@ public class FileControlled {
             int inviteCode = fileSharer.offerFile(targetPath.toString());
 
             // Start the file server for the uploaded file asynchronously
-            fileSharer.startFileServer(inviteCode + "csdsdssfdcdscsdfscxfddda");
+            fileSharer.startFileServer(inviteCode + "csdssdfscxfddda");
 
             Map<String, Object> response = new HashMap<>();
             response.put("inviteCode", inviteCode + "hbhbubb");
